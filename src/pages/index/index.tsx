d52439c117de8215b854e8c767323e4ca84d36b4
ccj <363992836@qq.com>
2022-07-07 11:06:24 +0800

    refactor(ui): 更换ui为antm

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,65 +1,25 @@
-import React, { useEffect } from 'react'
-import { Button, ConfigProvider } from '@taroify/core'
-import styles from './index.module.scss'
-import { View } from '@tarojs/components'
-import { useSelector, useDispatch } from 'react-redux'
-import cs from 'classnames'
-import { Router } from 'tarojs-router-next'
-import Counter from './components/Counter'
-import { addUser, deleteUser } from '@/store/actions/user'
-import IconFont from '@/assets/icons'
-import BaseButton from '@/components/BaseButton'
+import * as React from 'react'
 import demoApi from '@/api/demo'
-import { REDUX } from '@/store/REDUX'
+import { Button } from '@antmjs/vantui'
+
 const Index: React.FC = () => {
-  const { num } = useSelector((state: REDUX.RootState) => state.user)
-  const dispatch = useDispatch()
-  useEffect(() => {
-    demoApi.getUserInfo().then(res => {
-      console.log('res :>> ', res)
-    }).catch(err => {
-      console.log('err :>> ', err)
-    })
-  }, [])
-  const jumpToDemo = (demoType: 'router') => {
-    switch (demoType) {
-      case 'router':
-        Router.packageA.toIndex({
-          params: { id: 111, name: 'ccj' }, data: {
-            users: [{
-              id: 22,
-              name: '11'
-            }]
-          }
-        })
-    }
-  }
-  const testClass = cs('flex justify-center items-center', `${styles.test}`)
-  return (
-    <>
-      <ConfigProvider theme={{
-        buttonBackgroundColorPrimary: '#07c160',
-      }}
-      >
-        <View className='mt-4'>
-          <Button onClick={() => jumpToDemo('router')} color='danger' block>路由跳转</Button>
-          <Button color='primary' disabled className='mt-6' block>
-            主要按钮
-          </Button>
-          <View className={testClass}>css module</View>
-          <View className='flex justify-between mt-6'>
-            <Button color='primary' onClick={() => dispatch(addUser(4))}>增加</Button>
-            <Button onClick={() => dispatch(deleteUser(1))}>删除</Button>
-          </View>
-          <Counter num={num} />
-          <IconFont name='zhuanfarenwu' size={100} />
-          <BaseButton content='button' />
-          <View className='taroify-ellipsis'>asgdhjkasghjasgdhjasghjdasghjdgasjhdgshjagdhjasgdhjasgdhjasgdhjasghjdassdgfjhasgdhjas</View>
-          <View />
-        </View>
-      </ConfigProvider>
-    </>
-  )
+	React.useEffect(() => {
+		demoApi
+			.getUserInfo()
+			.then(res => {
+				console.log('res :>> ', res)
+			})
+			.catch(err => {
+				console.log('err :>> ', err)
+			})
+	}, [])
+	return (
+		<>
+			<Button type='primary' block>
+				默认按钮
+			</Button>
+		</>
+	)
 }
 
 export default Index
